52df58abfb69d36d0b884e925ac09ead52b32ee9
Fred Brooks <fred@sma2.rain.com>
2016-10-09 18:38:24 -0700

    update

---
+++ vtouch18f8722/vtouch_8722.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=vtouch_8722.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/vtouch_8722.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=vtouch8722.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/vtouch8722.x.tar
