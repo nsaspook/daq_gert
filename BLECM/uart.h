2dbc0d27c3d5eb886d758ce93adbd07926ab726d
Fred Brooks <nsaspook@nsaspook.com>
2016-10-17 21:14:48 -0700

    source update

---
+++ BLECM/uart.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2014 Microchip Technology Inc. and its subsidiaries.  You may use this software and any derivatives
+ * exclusively with Microchip products.
+ *
+ * MICROCHIP SOFTWARE NOTICE AND DISCLAIMER:  You may use this software, and any derivatives created by any person or
+ * entity by or on your behalf, exclusively with Microchip?s products.  Microchip and its licensors retain all ownership
+ * and intellectual property rights in the accompanying software and in all derivatives hereto.
+ *
+ * This software and any accompanying information is for suggestion only.  It does not modify Microchip?s standard
+ * warranty for its products.  You agree that you are solely responsible for testing the software and determining its
+ * suitability.  Microchip has no obligation to modify, test, certify, or support the software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING,
+ * BUT NOT LIMITED TO, IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A PARTICULAR PURPOSE
+ * APPLY TO THIS SOFTWARE, ITS INTERACTION WITH MICROCHIP?S PRODUCTS, COMBINATION WITH ANY OTHER PRODUCTS, OR USE IN
+ * ANY APPLICATION.
+ *
+ * IN NO EVENT, WILL MICROCHIP BE LIABLE, WHETHER IN CONTRACT, WARRANTY, TORT (INCLUDING NEGLIGENCE OR BREACH OF
+ * STATUTORY DUTY), STRICT LIABILITY, INDEMNITY, CONTRIBUTION, OR OTHERWISE, FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+ * EXEMPLARY, INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, FOR COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE
+ * SOFTWARE, HOWSOEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.
+ * TO THE FULLEST EXTENT ALLOWABLE BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO THIS
+ * SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE TERMS.
+ *
+ *
+ * File:        uart.h
+ * Date:        January 20, 2015
+ * Compiler:    XC16 v1.23
+ *
+ * Uart functions
+ *
+ */
+#ifndef UART_H
+#define UART_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+void UART_Init(void);
+inline void UART_TxStart(void);
+bool UART_IsNewRxData(void);
+uint8_t UART_ReadRxBuffer(void);
+void UART_WriteTxBuffer(const uint8_t);
+uint16_t UART_GetTXBufferFreeSpace(void);
+uint8_t UART_PeekRxBuffer(void);
+
+#endif
