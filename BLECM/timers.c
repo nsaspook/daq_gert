db22072c8f9ed5e7710514bbba3812cf8a50d5e6
Fred Brooks <nsaspook@nsaspook.com>
2016-10-18 20:58:13 -0700

    source update

--- BLECM/timers.c
+++ BLECM/timers.c
@@ -46,102 +46,115 @@ static volatile uint16_t tickCount[TMR_COUNT] = {0};
 
 //**********************************************************************************************************************
 // Initialize the timers
+
 void Timers_Init(void)
 {
-    //Timer 5 is used for interrupt based software timers counting 1ms intervals to a resolution of 500us
-    T5CON = TIMER_OFF;                      //Timer 5 off
-    TMR5 = 0;                               //Clear timer 5
-    PR5 = TIMER_500US_PERIOD;               //Set the period value for 500us
-    T5CON |= TIMER_ON_PRESCALE1;            //using 1:1 prescaler and turn on timer 5
-    IFS1bits.T5IF = 0;                      //Clear the interrupt flag
-    IEC1bits.T5IE = 1;                      //Enable the timer 5 interrupt
-    
+	//Timer 5 is used for interrupt based software timers counting 1ms intervals to a resolution of 500us
+	T5CON = TIMER_OFF; //Timer 5 off
+	TMR5 = 0; //Clear timer 5
+	PR5 = TIMER_500US_PERIOD; //Set the period value for 500us
+	T5CON |= TIMER_ON_PRESCALE1; //using 1:1 prescaler and turn on timer 5
+	IFS1bits.T5IF = 0; //Clear the interrupt flag
+	IEC1bits.T5IE = 1; //Enable the timer 5 interrupt
+
 #ifdef USE_SLEEP                //see config.h, Application setting section
-    //Timer 2/3 is used in 32-bit mode as inactivity timer to trigger sleep mode
-    T2CON = TIMER_OFF;                      //Timer 3 off
-    TMR3 = 0;                               //Clear timer 3
-    TMR2 = 0;                               //Clear timer 2
-    T2CONbits.T32 = 1;                      //Enable 32-bit mode
-    PR3 = (uint16_t)(SLEEP_TIME >> 16);               //Set the period value - msw
-    PR2 = (uint16_t)(SLEEP_TIME | 0x0000FFFF);        //lsw
-    T2CON |= TIMER_ON_PRESCALE256;          //using 1:256 prescaler and turn on timer 3
-    IFS0bits.T3IF = 0;                      //Clear the interrupt flag
-    IEC0bits.T3IE = 1;                      //Enable the timer 3 interrupt
-    
+	//Timer 2/3 is used in 32-bit mode as inactivity timer to trigger sleep mode
+	T2CON = TIMER_OFF; //Timer 3 off
+	TMR3 = 0; //Clear timer 3
+	TMR2 = 0; //Clear timer 2
+	T2CONbits.T32 = 1; //Enable 32-bit mode
+	PR3 = (uint16_t) (SLEEP_TIME >> 16); //Set the period value - msw
+	PR2 = (uint16_t) (SLEEP_TIME | 0x0000FFFF); //lsw
+	T2CON |= TIMER_ON_PRESCALE256; //using 1:256 prescaler and turn on timer 3
+	IFS0bits.T3IF = 0; //Clear the interrupt flag
+	IEC0bits.T3IE = 1; //Enable the timer 3 interrupt
+
 #ifndef SLEEP_MODE_RTCC         //we'll be using Timer 1 for periodic wakeup
-    T1CON = 0x0000;                         //Timer 1 off
-    T1CONbits.T1ECS = 0b10;                 //Clock source LPRC
-    T1CONbits.TCS = 1;
-    T1CONbits.TCKPS = 0b11;                 //using 1:256 prescaler
-    TMR1 = 0;                               //Clear timer 1
-    PR1 = T1_SLEEP_PERIOD;                  //Set the period for sleep
-    IFS0bits.T1IF = 0;                      //Clear the interrupt flag
-    IEC0bits.T1IE = 1;                      //Enable the timer 1 interrupt
+	T1CON = 0x0000; //Timer 1 off
+	T1CONbits.T1ECS = 0b10; //Clock source LPRC
+	T1CONbits.TCS = 1;
+	T1CONbits.TCKPS = 0b11; //using 1:256 prescaler
+	TMR1 = 0; //Clear timer 1
+	PR1 = T1_SLEEP_PERIOD; //Set the period for sleep
+	IFS0bits.T1IF = 0; //Clear the interrupt flag
+	IEC0bits.T1IE = 1; //Enable the timer 1 interrupt
 #endif //not SLEEP_MODE_RTCC    
 #endif //USE_SLEEP
 }
 
 //**********************************************************************************************************************
 // Start one of the software timers
+
 inline void StartTimer(uint8_t timer, uint16_t count)
 {
-    tickCount[timer] = count << 1; //Interrupt is every 500us but StartTimer() takes multiple of 1ms so multiply by 2
+	tickCount[timer] = count << 1; //Interrupt is every 500us but StartTimer() takes multiple of 1ms so multiply by 2
 }
 
 //**********************************************************************************************************************
 // Check if one of the software software timers has timed out
+
 inline bool TimerDone(uint8_t timer)
 {
-    if(tickCount[timer] == 0) {             //Check if counted down to zero
-        return true;                        //then return true
-    }
-    return false;                           //else return false
+	if (tickCount[timer] == 0) { //Check if counted down to zero
+		return true; //then return true
+	}
+	return false; //else return false
 }
 
 //**********************************************************************************************************************
 // Simple delay for n milliseconds (blocking)
+
 void WaitMs(uint16_t numMilliseconds)
 {
-    StartTimer(TMR_INTERNAL, numMilliseconds);         //Start software timer and wait for it to count down
-    while(!TimerDone(TMR_INTERNAL)) {Idle();}          //Enter idle mode to reduce power while waiting
-}                                                      //(timer interrupt will wake part from idle)
+	StartTimer(TMR_INTERNAL, numMilliseconds); //Start software timer and wait for it to count down
+	while (!TimerDone(TMR_INTERNAL)) {
+		Idle();
+	} //Enter idle mode to reduce power while waiting
+} //(timer interrupt will wake part from idle)
 
 #ifdef USE_SLEEP                //see config.h, Application setting section
 //Reset the inactivity sleep timer
-inline void SleepTimerReset(void) {
-    TMR3 = 0;                               //Clear timer 3
-    TMR2 = 0;                               //Clear timer 2
+
+inline void SleepTimerReset(void)
+{
+	TMR3 = 0; //Clear timer 3
+	TMR2 = 0; //Clear timer 2
 }
 #endif
 
 //**********************************************************************************************************************
 // Timer 5 interrupt routine - software timers
+
 void _ISR_NO_AUTO_PSV _T5Interrupt(void)
 {
-    uint8_t i;
+	uint8_t i;
 
-    IFS1bits.T5IF = 0;                      //Clear the interrupt flag
-    //Decrement each software timer
-    for(i = 0; i < TMR_COUNT; i++) {
-        if(tickCount[i] != 0) {tickCount[i]--;}
-    }
+	IFS1bits.T5IF = 0; //Clear the interrupt flag
+	//Decrement each software timer
+	for (i = 0; i < TMR_COUNT; i++) {
+		if (tickCount[i] != 0) {
+			tickCount[i]--;
+		}
+	}
 }
 
 #ifdef USE_SLEEP                //see config.h, Application setting section
 //**********************************************************************************************************************
 // Timer 3 interrupt routine - inactivity timer
+
 void _ISR_NO_AUTO_PSV _T3Interrupt(void)
 {
-    IFS0bits.T3IF = 0;                      //Clear the interrupt flag    
-    appData.sleepFlag = true;
+	IFS0bits.T3IF = 0; //Clear the interrupt flag    
+	appData.sleepFlag = true;
 }
 #ifndef SLEEP_MODE_RTCC
 //**********************************************************************************************************************
 // Timer 1 interrupt routine - periodic wakeup timer
+
 void _ISR_NO_AUTO_PSV _T1Interrupt(void)
 {
-    IFS0bits.T1IF = 0;                      //Clear the interrupt flag
-    appData.timer1Flag = true;
+	IFS0bits.T1IF = 0; //Clear the interrupt flag
+	appData.timer1Flag = true;
 }
 #endif //not SLEEP_MODE_RTCC
 #endif //USE_SLEEP
--- BLECM/uart.c
+++ BLECM/uart.c
@@ -41,19 +41,21 @@
 #include "config.h"
 
 //UART receive buffer type
+
 typedef struct {
-    volatile uint8_t buffer[SIZE_RxBuffer];
-    volatile uint8_t volatile *head;
-    volatile uint8_t *tail;
-    volatile uint16_t byteCount;
+	volatile uint8_t buffer[SIZE_RxBuffer];
+	volatile uint8_t volatile *head;
+	volatile uint8_t *tail;
+	volatile uint16_t byteCount;
 } UART_RX_BUFFER_T;
 
 //UART transmit buffer type
+
 typedef struct {
-    volatile uint8_t buffer[SIZE_TxBuffer];
-    volatile uint8_t *head;
-    volatile uint8_t volatile *tail;
-    volatile uint16_t byteCount;
+	volatile uint8_t buffer[SIZE_TxBuffer];
+	volatile uint8_t *head;
+	volatile uint8_t volatile *tail;
+	volatile uint16_t byteCount;
 } UART_TX_BUFFER_T;
 
 //Buffer instances
@@ -62,156 +64,167 @@ static UART_TX_BUFFER_T txBuf;
 
 //**********************************************************************************************************************
 // Initialize the UART to communicate with the Bluetooth module
+
 void UART_Init(void)
 {
-    rxBuf.tail = &rxBuf.buffer[0];     //Initialize the pointers
-    rxBuf.head = &rxBuf.buffer[0];
-    txBuf.tail = &txBuf.buffer[0];
-    txBuf.head = &txBuf.buffer[0];
-    txBuf.byteCount = rxBuf.byteCount = 0;
-    
-    U1BRG = 34;                             //Baud rate 115,200 - actually 115,384 baud 0.16% error
-    U1MODE = 0x8008;                        //Enable UART, no flow control, BRGH = 1 for high speed baud mode
-    U1STA = 0x0400;                         //Enable transmit
-    UART_RX_IF = 0;                         //Clear UART interrupt flags
-    UART_ER_IF = 0;
-    UART_RX_IE = 1;                         //Enable UART Receive and Error interrupt
-    UART_ER_IE = 1;
+	rxBuf.tail = &rxBuf.buffer[0]; //Initialize the pointers
+	rxBuf.head = &rxBuf.buffer[0];
+	txBuf.tail = &txBuf.buffer[0];
+	txBuf.head = &txBuf.buffer[0];
+	txBuf.byteCount = rxBuf.byteCount = 0;
+
+	U1BRG = 34; //Baud rate 115,200 - actually 115,384 baud 0.16% error
+	U1MODE = 0x8008; //Enable UART, no flow control, BRGH = 1 for high speed baud mode
+	U1STA = 0x0400; //Enable transmit
+	UART_RX_IF = 0; //Clear UART interrupt flags
+	UART_ER_IF = 0;
+	UART_RX_IE = 1; //Enable UART Receive and Error interrupt
+	UART_ER_IE = 1;
 }
 
 //**********************************************************************************************************************
 // Start transmission by enabling the UART transmit interrupt
+
 inline void UART_TxStart(void)
 {
-    UART_TX_IE = 1;                    //Enable transmit interrupts
+	UART_TX_IE = 1; //Enable transmit interrupts
 }
 
 //**********************************************************************************************************************
 // See if there are one or more bytes in the receive buffer
+
 bool UART_IsNewRxData(void)
 {
-    __builtin_disi(0x3FFF);             //disable interrupts
-    if(rxBuf.byteCount == 0) {          //Check if data in buffer
-        __builtin_disi(0);              //enable interrupts
-        return(false);                  //No bytes in the buffer so return false
-    }
-    __builtin_disi(0);          //enable interrupts
-    return(true);               //There are bytes in the buffer
+	__builtin_disi(0x3FFF); //disable interrupts
+	if (rxBuf.byteCount == 0) { //Check if data in buffer
+		__builtin_disi(0); //enable interrupts
+		return(false); //No bytes in the buffer so return false
+	}
+	__builtin_disi(0); //enable interrupts
+	return(true); //There are bytes in the buffer
 }
 
 //**********************************************************************************************************************
 // Read a byte from the receive buffer
+
 uint8_t UART_ReadRxBuffer(void)
 {
-    uint8_t Temp;
+	uint8_t Temp;
 
-    __builtin_disi(0x3FFF);     //disable interrupts
-    if(rxBuf.byteCount == 0) {              //For safety, do not allow read of empty buffer
-        __builtin_disi(0);                  //enable interrupts
-        return(0);                          //Return zero if there is nothing in the buffer
-    }
+	__builtin_disi(0x3FFF); //disable interrupts
+	if (rxBuf.byteCount == 0) { //For safety, do not allow read of empty buffer
+		__builtin_disi(0); //enable interrupts
+		return(0); //Return zero if there is nothing in the buffer
+	}
 
-    rxBuf.byteCount--;          //Decrement byte count
-    __builtin_disi(0);          //enable interrupts
-    Temp = *rxBuf.tail++;                   //Get the byte and increment the pointer
-    if (rxBuf.tail > &rxBuf.buffer[SIZE_RxBuffer - 1]) { //Check if at end of buffer
-        rxBuf.tail = &rxBuf.buffer[0];          //then wrap the pointer to beginning
-    }
-    return(Temp);
+	rxBuf.byteCount--; //Decrement byte count
+	__builtin_disi(0); //enable interrupts
+	Temp = *rxBuf.tail++; //Get the byte and increment the pointer
+	if (rxBuf.tail > &rxBuf.buffer[SIZE_RxBuffer - 1]) { //Check if at end of buffer
+		rxBuf.tail = &rxBuf.buffer[0]; //then wrap the pointer to beginning
+	}
+	return(Temp);
 }
 
 //**********************************************************************************************************************
 // Write a byte to the transmit buffer
+
 void UART_WriteTxBuffer(const uint8_t TxByte)
 {
-    *txBuf.head++ = TxByte;                 //Put the byte in the transmit buffer and increment the pointer
-    if (txBuf.head > &txBuf.buffer[SIZE_TxBuffer - 1]) { //Check if at end of buffer
-        txBuf.head = &txBuf.buffer[0];          //Wrap pointer to beginning
-    }
-    __builtin_disi(0x3FFF);     //disable interrupts
-    if(txBuf.byteCount < SIZE_TxBuffer) {     //Increment byte count
-        txBuf.byteCount++;
-    }
-    __builtin_disi(0);    //enable interrupts
+	*txBuf.head++ = TxByte; //Put the byte in the transmit buffer and increment the pointer
+	if (txBuf.head > &txBuf.buffer[SIZE_TxBuffer - 1]) { //Check if at end of buffer
+		txBuf.head = &txBuf.buffer[0]; //Wrap pointer to beginning
+	}
+	__builtin_disi(0x3FFF); //disable interrupts
+	if (txBuf.byteCount < SIZE_TxBuffer) { //Increment byte count
+		txBuf.byteCount++;
+	}
+	__builtin_disi(0); //enable interrupts
 }
 
 //**********************************************************************************************************************
 // Return the number of bytes free in the TX buffer
-uint16_t UART_GetTXBufferFreeSpace(void) {
-    uint16_t space;
 
-    __builtin_disi(0x3FFF);     //disable interrupts            
-    space = SIZE_TxBuffer - txBuf.byteCount;
-    __builtin_disi(0);          //enable interrupts
-    return space;
+uint16_t UART_GetTXBufferFreeSpace(void)
+{
+	uint16_t space;
+
+	__builtin_disi(0x3FFF); //disable interrupts            
+	space = SIZE_TxBuffer - txBuf.byteCount;
+	__builtin_disi(0); //enable interrupts
+	return space;
 }
 
 //Peek at buffer tail
-uint8_t UART_PeekRxBuffer(void) {
-    __builtin_disi(0x3FFF);     //disable interrupts
-    if(rxBuf.byteCount == 0) {              //Check if pointers are the same
-        __builtin_disi(0);                  //enable interrupts
-        return(NULL);                      //No bytes in the buffer so return NULL
-    }
-    else {
-        __builtin_disi(0);                  //enable interrupts
-        return *rxBuf.tail;
-    }
+
+uint8_t UART_PeekRxBuffer(void)
+{
+	__builtin_disi(0x3FFF); //disable interrupts
+	if (rxBuf.byteCount == 0) { //Check if pointers are the same
+		__builtin_disi(0); //enable interrupts
+		return(NULL); //No bytes in the buffer so return NULL
+	} else {
+		__builtin_disi(0); //enable interrupts
+		return *rxBuf.tail;
+	}
 }
 
 //**********************************************************************************************************************
 // Interrupt routine for UART receive interrupts
+
 void __attribute__((interrupt, no_auto_psv)) _U1RXInterrupt(void)
 {
-    UART_RX_IF = 0;                    //Clear UART Receive interrupt flag
-    *rxBuf.head++ = UART_RX_BUF;       //Put received byte in the buffer
-    if (rxBuf.head > &rxBuf.buffer[SIZE_RxBuffer - 1]) {  //Check if end of buffer
-        rxBuf.head = &rxBuf.buffer[0];     //Wrap pointer to beginning
-    }
-    if(rxBuf.byteCount < SIZE_RxBuffer) {     //Increment byte count
-        rxBuf.byteCount++;
-    }
+	UART_RX_IF = 0; //Clear UART Receive interrupt flag
+	*rxBuf.head++ = UART_RX_BUF; //Put received byte in the buffer
+	if (rxBuf.head > &rxBuf.buffer[SIZE_RxBuffer - 1]) { //Check if end of buffer
+		rxBuf.head = &rxBuf.buffer[0]; //Wrap pointer to beginning
+	}
+	if (rxBuf.byteCount < SIZE_RxBuffer) { //Increment byte count
+		rxBuf.byteCount++;
+	}
 }
 
 //**********************************************************************************************************************
 // Interrupt routine for UART transmit interrupts
+
 void __attribute__((interrupt, no_auto_psv)) _U1TXInterrupt(void)
 {
-    if(txBuf.byteCount > 0)           //Check if more data is in the buffer
-    {
-        //Only do anything if hardware buffer has space
-        if(!UART_FULL) {
-            UART_TX_IF = 0;                         //Clear UART 1 TX interrupt flag
-            UART_TX_BUF = *txBuf.tail++;            //Load next byte into the TX buffer
-            if (txBuf.tail > &txBuf.buffer[SIZE_TxBuffer - 1]) {  //Check if end of buffer
-                txBuf.tail = &txBuf.buffer[0];      //Wrap pointer to beginning
-            }
-            txBuf.byteCount--;     //Decrement byte count
-        }
-    }
-    else {
-        UART_TX_IE = 0;                //No more data to transmit, so stop interrupts
-    }
+	if (txBuf.byteCount > 0) //Check if more data is in the buffer
+	{
+		//Only do anything if hardware buffer has space
+		if (!UART_FULL) {
+			UART_TX_IF = 0; //Clear UART 1 TX interrupt flag
+			UART_TX_BUF = *txBuf.tail++; //Load next byte into the TX buffer
+			if (txBuf.tail > &txBuf.buffer[SIZE_TxBuffer - 1]) { //Check if end of buffer
+				txBuf.tail = &txBuf.buffer[0]; //Wrap pointer to beginning
+			}
+			txBuf.byteCount--; //Decrement byte count
+		}
+	} else {
+		UART_TX_IE = 0; //No more data to transmit, so stop interrupts
+	}
 }
 
 //**********************************************************************************************************************
 // Interrupt routine for UART error interrupts
-void __attribute__((interrupt, no_auto_psv)) _U1ErrInterrupt(void) {
-    IFS4bits.U1ERIF = 0;        //Clear interrupt flag
-    
-    //Handle an overflow error by reading next byte and clearing flag
-    if(U1STAbits.OERR == 1) {
-        *rxBuf.head++ = UART_RX_BUF;       //Put received byte in the buffer
-        if (rxBuf.head > &rxBuf.buffer[SIZE_RxBuffer - 1]) {  //Check if end of buffer
-            rxBuf.head = &rxBuf.buffer[0];     //Wrap pointer to beginning
-        }
-        if(rxBuf.byteCount < SIZE_RxBuffer) {     //Increment byte count
-            rxBuf.byteCount++;
-        }
-        U1STAbits.OERR = 0;
-    }
 
-    //Clear any other error bits
-    U1STAbits.FERR = 0;
-    U1STAbits.PERR = 0;
+void __attribute__((interrupt, no_auto_psv)) _U1ErrInterrupt(void)
+{
+	IFS4bits.U1ERIF = 0; //Clear interrupt flag
+
+	//Handle an overflow error by reading next byte and clearing flag
+	if (U1STAbits.OERR == 1) {
+		*rxBuf.head++ = UART_RX_BUF; //Put received byte in the buffer
+		if (rxBuf.head > &rxBuf.buffer[SIZE_RxBuffer - 1]) { //Check if end of buffer
+			rxBuf.head = &rxBuf.buffer[0]; //Wrap pointer to beginning
+		}
+		if (rxBuf.byteCount < SIZE_RxBuffer) { //Increment byte count
+			rxBuf.byteCount++;
+		}
+		U1STAbits.OERR = 0;
+	}
+
+	//Clear any other error bits
+	U1STAbits.FERR = 0;
+	U1STAbits.PERR = 0;
 }
