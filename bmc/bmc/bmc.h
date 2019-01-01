393ef45d3f124b44ba54c5fdbab5cd18d50a68ce
Fred Brooks <fred@sma2.rain.com>
2019-01-01 10:53:20 -0800

    updates

--- bmc/bmc/bmc.h
+++ bmc/bmc/bmc.h
@@ -15,32 +15,32 @@ extern "C" {
 #define TRUE    1
 #define FALSE   0
 
-    struct didata {
-        uint32_t D0 : 1; // 
-        uint32_t D1 : 1; // 
-        uint32_t D2 : 1; // 
-        uint32_t D3 : 1; // 
-        uint32_t D4 : 1; // 
-        uint32_t D5 : 1; // 
-        uint32_t D6 : 1; // 
-        uint32_t D7 : 1; // 
-    } volatile ditype;
+	struct didata {
+		uint32_t D0 : 1; // 
+		uint32_t D1 : 1; // 
+		uint32_t D2 : 1; // 
+		uint32_t D3 : 1; // 
+		uint32_t D4 : 1; // 
+		uint32_t D5 : 1; // 
+		uint32_t D6 : 1; // 
+		uint32_t D7 : 1; // 
+	} volatile ditype;
 
-    union dio_buf_type {
-        uint32_t dio_buf;
-        struct didata d;
-    };
+	union dio_buf_type {
+		uint32_t dio_buf;
+		struct didata d;
+	};
 
-    typedef struct bmcdata {
-        double pv_voltage, cc_voltage, input_voltage, b1_voltage, b2_voltage, system_voltage, logic_voltage;
-        double pv_current, cc_current, battery_current;
-        struct didata datain;
-        union dio_buf_type dataout;
-        int32_t adc_sample[32];
-        int32_t dac_sample[32];
-        int32_t utc;
-    }
-    volatile bmctype;
+	typedef struct bmcdata {
+		double pv_voltage, cc_voltage, input_voltage, b1_voltage, b2_voltage, system_voltage, logic_voltage;
+		double pv_current, cc_current, battery_current;
+		struct didata datain;
+		union dio_buf_type dataout;
+		int32_t adc_sample[32];
+		int32_t dac_sample[32];
+		int32_t utc;
+	}
+	volatile bmctype;
 
 #ifdef __cplusplus
 }
--- bmc/bmc/daq.h
+++ bmc/bmc/daq.h
@@ -29,40 +29,40 @@ extern "C" {
 #include <comedilib.h>
 #include "bmc.h"
 
-    int subdev_ai; /* change this to your input subdevice */
-    int chan_ai; /* change this to your channel */
-    int range_ai; /* more on this later */
-    int aref_ai; /* more on this later */
-    int maxdata_ai, ranges_ai, channels_ai;
+	int subdev_ai; /* change this to your input subdevice */
+	int chan_ai; /* change this to your channel */
+	int range_ai; /* more on this later */
+	int aref_ai; /* more on this later */
+	int maxdata_ai, ranges_ai, channels_ai;
 
-    int subdev_dio; /* change this to your input subdevice */
-    int chan_dio; /* change this to your channel */
-    int range_dio; /* more on this later */
-    int aref_dio; /* more on this later */
-    int maxdata_dio, ranges_dio, channels_dio, datain_dio;
+	int subdev_dio; /* change this to your input subdevice */
+	int chan_dio; /* change this to your channel */
+	int range_dio; /* more on this later */
+	int aref_dio; /* more on this later */
+	int maxdata_dio, ranges_dio, channels_dio, datain_dio;
 
-    comedi_t *it;
-    comedi_range *ad_range;
-    int8_t ADC_OPEN, DIO_OPEN, ADC_ERROR, DEV_OPEN, DIO_ERROR;
+	comedi_t *it;
+	comedi_range *ad_range;
+	int8_t ADC_OPEN, DIO_OPEN, ADC_ERROR, DEV_OPEN, DIO_ERROR;
 
-    extern volatile struct bmcdata bmc;
-    extern struct didata datain;
-    extern struct dodata dataout;
+	extern volatile struct bmcdata bmc;
+	extern struct didata datain;
+	extern struct dodata dataout;
 
-    int init_daq(double, double, int);
-    int init_dac(double, double, int);
-    int init_dio(void);
-    int adc_range(double, double);
-    int dac_range(double, double);
-    double get_adc_volts(int);
-    int set_dac_volts(int, double);
-    int set_dac_raw(int, lsampl_t);
-    int get_dio_bit(int);
-    int put_dio_bit(int, int);
-    int set_dio_input(int);
-    int set_dio_output(int);
-    int get_data_sample(void);
-    double lp_filter(double, int, int);
+	int init_daq(double, double, int);
+	int init_dac(double, double, int);
+	int init_dio(void);
+	int adc_range(double, double);
+	int dac_range(double, double);
+	double get_adc_volts(int);
+	int set_dac_volts(int, double);
+	int set_dac_raw(int, lsampl_t);
+	int get_dio_bit(int);
+	int put_dio_bit(int, int);
+	int set_dio_input(int);
+	int set_dio_output(int);
+	int get_data_sample(void);
+	double lp_filter(double, int, int);
 #ifdef __cplusplus
 }
 #endif
