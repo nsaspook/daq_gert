77c87bb4cac2e129bcc7d06a14be43a4b2a90547
nsaspook <nsaspook@nsaspook.com>
2024-01-21 13:12:42 -0800

    up

--- bmc/bmc.c
+++ bmc/bmc.c
@@ -35,17 +35,24 @@
 
 #define DAQ_STR 32
 #define DAQ_STR_M DAQ_STR-1
-#define MQTT_TIMEOUT    100
+#define MQTT_TIMEOUT    150
 
-#define MQTT_VERSION  "v0.2"
+#define MQTT_VERSION  "v0.3"
 
 volatile MQTTClient_deliveryToken deliveredtoken, receivedtoken = false;
 volatile bool runner = false;
+uint8_t led_buf = 0;
 
 void timer_callback(int sig);
 void delivered(void *, MQTTClient_deliveryToken);
 int msgarrvd(void *, char *, int, MQTTClient_message *);
 void connlost(void *, char *);
+
+void led_lightshow(int32_t);
+
+/*
+ * async control threads
+ */
 
 /*
  * Comedi data update timer flag
@@ -76,6 +83,7 @@ void connlost(void *context, char *cause)
 
 	printf("\nConnection lost\n");
 	printf("     cause: %s\n", cause);
+	exit(EXIT_FAILURE);
 }
 
 char *token;
@@ -83,16 +91,14 @@ cJSON *json;
 
 int main(int argc, char *argv[])
 {
-	uint8_t j = 75;
 	uint32_t sequence = 0;
 
-	struct itimerval new_timer;
-	struct itimerval old_timer;
-
-	new_timer.it_value.tv_sec = 1;
-	new_timer.it_value.tv_usec = 0;
-	new_timer.it_interval.tv_sec = 1;
-	new_timer.it_interval.tv_usec = 0;
+	struct itimerval new_timer = {
+		.it_value.tv_sec = 1,
+		.it_value.tv_usec = 0,
+		.it_interval.tv_sec = 0,
+		.it_interval.tv_usec = 100000,
+	}, old_timer;
 
 	setitimer(ITIMER_REAL, &new_timer, &old_timer);
 	signal(SIGALRM, timer_callback);
@@ -101,13 +107,12 @@ int main(int argc, char *argv[])
 	MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
 	MQTTClient_message pubmsg = MQTTClient_message_initializer;
 	MQTTClient_deliveryToken token;
-	int rc;
+	int32_t rc;
 
 	MQTTClient_create(&client, ADDRESS, CLIENTID,
 		MQTTCLIENT_PERSISTENCE_NONE, NULL);
 	conn_opts.keepAliveInterval = 20;
 	conn_opts.cleansession = 1;
-
 
 	MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
 	if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
@@ -122,15 +127,21 @@ int main(int argc, char *argv[])
 	while (true) {
 		if (runner) {
 			runner = false;
+			led_lightshow(1); // roll remote LED bits from led_buf
 			json = cJSON_CreateObject();
 
 			cJSON_AddStringToObject(json, "Name", "HA_comedi_set");
 			cJSON_AddNumberToObject(json, "Sequence_set", sequence++);
 			cJSON_AddNumberToObject(json, "DAC0", 1.234);
 			cJSON_AddNumberToObject(json, "DAC1", 2.345);
-			cJSON_AddNumberToObject(json, "DO0", 1);
-			cJSON_AddNumberToObject(json, "DO5", 0);
-			cJSON_AddNumberToObject(json, "DO7", j++&0x1);
+			cJSON_AddNumberToObject(json, "DO0", led_buf >> 0 & 1);
+			cJSON_AddNumberToObject(json, "DO1", led_buf >> 1 & 1);
+			cJSON_AddNumberToObject(json, "DO2", led_buf >> 2 & 1);
+			cJSON_AddNumberToObject(json, "DO3", led_buf >> 3 & 1);
+			cJSON_AddNumberToObject(json, "DO4", led_buf >> 4 & 1);
+			cJSON_AddNumberToObject(json, "DO5", led_buf >> 5 & 1);
+			cJSON_AddNumberToObject(json, "DO6", led_buf >> 6 & 1);
+			cJSON_AddNumberToObject(json, "DO7", led_buf >> 7 & 1);
 			cJSON_AddStringToObject(json, "System", "K8055/VM110");
 			// convert the cJSON object to a JSON string
 			char *json_str = cJSON_Print(json);
@@ -159,6 +170,40 @@ int main(int argc, char *argv[])
 		}
 	}
 	return 0;
+}
+
+void led_lightshow(int32_t speed)
+{
+	static int32_t j = 0;
+	static uint8_t cylon = 0xff;
+	static int32_t alive_led = 0;
+	static bool LED_UP = true;
+
+	if (++j >= speed) { // delay a bit ok
+		if (false) { // screen status feedback
+			led_buf = ~cylon; // roll leds cylon style
+		} else {
+			led_buf = cylon; // roll leds cylon style (inverted)
+		}
+
+		if (LED_UP && (alive_led != 0)) {
+			alive_led = alive_led * 2;
+			cylon = cylon << 1;
+		} else {
+			if (alive_led != 0) alive_led = alive_led / 2;
+			cylon = cylon >> 1;
+		}
+		if (alive_led < 2) {
+			alive_led = 2;
+			LED_UP = true;
+		} else {
+			if (alive_led > 128) {
+				alive_led = 128;
+				LED_UP = false;
+			}
+		}
+		j = 0;
+	}
 }
 
 
